caff99593407a04ceb0e9e0756bf6707f8b80112
lake <102535481@qq.com>
2025-11-14 03:00:48 +0800

    使用本地fast-stream

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,17 +4,18 @@ use std::fs;
 use std::fs::OpenOptions;
 use std::time::Instant;
 use fast_stream::stream::Stream;
+// use fast_stream::stream::Stream;
 
 fn main() {
     let time = Instant::now();
-    let data = fs::read("./data/hi.png").unwrap();
+    let data = fs::read("../data/AppIcon160x60@2x.png").unwrap();
     let input = Stream::new(data.into());
     let output_file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .truncate(true)
-        .open("./data/origin2.png")
+        .open("../data/origin.png")
         .unwrap();
     let mut output = Stream::new(output_file.into());
     Png::restore(input, &mut output).unwrap();
